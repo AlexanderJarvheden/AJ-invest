862996033b98724251e861097d8e5319294d25c4
AlexanderJarvheden <alexander.jarvheden@gmail.com>
2024-07-25 20:34:50 +0200

    Make flask ui runnable

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 from flaskwebgui import FlaskUI
 
-app = Flask(static_folder="client/static", template_folder="client/templates")
-ui = FlaskUI(app)
+app = Flask(__name__, static_folder="client/static", template_folder="client/templates")
+ui = FlaskUI(app=app, server="flask")
 
 @app.route("/")
 def home():
-    render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     ui.run()
